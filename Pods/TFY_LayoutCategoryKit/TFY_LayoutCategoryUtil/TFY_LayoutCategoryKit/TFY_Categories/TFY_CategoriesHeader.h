7a532a5baae0b3f0b838b2d9bbf764ec08111513
tianfengyou <420144542@qq.com>
2021-11-12 21:23:18 +0800

    优化

--- Pods/TFY_LayoutCategoryKit/TFY_LayoutCategoryUtil/TFY_LayoutCategoryKit/TFY_Categories/TFY_CategoriesHeader.h
+++ Pods/TFY_LayoutCategoryKit/TFY_LayoutCategoryUtil/TFY_LayoutCategoryKit/TFY_Categories/TFY_CategoriesHeader.h
@@ -15,5 +15,9 @@
 #import "TFY_FoundationHeader.h"
 //定义的一些宏
 #import "TFY_Define.h"
+//添加标签红点
+#import "TFY_BadgeViewHeader.h"
+//内联函数
+#import "TFY_Inlinefunction.h"
 
 #endif /* TFY_CategoriesHeader_h */
